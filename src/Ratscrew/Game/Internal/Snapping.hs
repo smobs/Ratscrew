2b198e8e17437dcf364dc6c7e6307833fc00108e
Toby Smyth <tobs169@gmail.com>
2015-05-27 23:57:25 +0100

    Count based snaps

--- src/Ratscrew/Game/Internal/Snapping.hs
+++ src/Ratscrew/Game/Internal/Snapping.hs
@@ -8,7 +8,11 @@ import Ratscrew.Cards
 import Control.Applicative
 
 isSnap :: Int -> [Card] -> Bool
-isSnap _ = (||) <$> darkQueen <*> normalSnap
+isSnap i = listOr [ darkQueen, normalSnap, sandwichSnap, matchesCount i]
+
+
+listOr :: [[Card] -> Bool] -> [Card] -> Bool
+listOr fs cs = any  (\f -> f cs) fs  
 
 darkQueen :: [Card] -> Bool
 darkQueen cs = case cs of
@@ -16,5 +20,29 @@ darkQueen cs = case cs of
                  _ -> False
 
 normalSnap :: [Card] -> Bool
-normalSnap cs = any f $ zip cs (drop 1 cs)
+normalSnap  = nthSnap 0
+
+sandwichSnap :: [Card] -> Bool
+sandwichSnap = nthSnap 1
+
+nthSnap :: Int -> [Card] -> Bool
+nthSnap i cs = any f $ zip cs (drop (i+1) cs)
                 where f (Card r1 _,Card r2 _) = r1 == r2 
+
+matchesCount :: Int -> [Card] -> Bool
+matchesCount i (Card r s : _) = case (i, r) of
+                           (1, Ace) -> True
+                           (2, Two) -> True
+                           (3, Three) -> True
+                           (4, Four) -> True
+                           (5, Five) -> True
+                           (6, Six) -> True
+                           (7, Seven) -> True
+                           (8, Eight) -> True
+                           (9, Nine) -> True
+                           (10, Ten) -> True
+                           (11, Jack) -> True
+                           (12, Queen) -> True
+                           (13, King) -> True
+                           _ -> False
+matchesCount _ _ = False
--- tests/Ratscrew/Game/Internal/Snapping/Tests.hs
+++ tests/Ratscrew/Game/Internal/Snapping/Tests.hs
@@ -20,7 +20,28 @@ properties =
                             , testProperty "Dark queen on top always snaps" darkQueen
                             , testProperty "Two side by side cards with the same rank is a snap." normalSnap
                             , testProperty "Two cards with the same rank one apart is a snap" sandwichSnap
+                            , testProperty "Top card matches the count" countMatchingSnap
                             ]
+
+
+countMatchingSnap :: [Card] -> (Rank, Suit) -> Bool
+countMatchingSnap cs (r, s) = fut (rankScore r) (Card r s : cs)
+
+rankScore :: Rank -> Int
+rankScore r = case r of
+                Ace -> 1
+                Two -> 2
+                Three -> 3
+                Four -> 4
+                Five -> 5
+                Six -> 6
+                Seven -> 7
+                Eight -> 8
+                Nine -> 9
+                Ten -> 10
+                Jack -> 11
+                Queen -> 12
+                King -> 13
 
 emptyProp :: Int -> Bool
 emptyProp i = not (fut i [])
@@ -39,6 +60,8 @@ oneApart (d1, c, d2) r (s1, s2) = d1 ++ [Card r s1, c, Card r s2] ++ d2
 
 singleSnap :: ([Card], [Card]) -> Rank -> (Suit, Suit) -> [Card]
 singleSnap (d1, d2)  r (s1, s2) = d1 ++ [Card r s1, Card r s2] ++ d2
+
+
 
 fut :: Int ->  [Card] -> Bool
 fut = Snapping.isSnap 
