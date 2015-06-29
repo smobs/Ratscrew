0571515df2c96cb152309baed90df280265fb872
Toby Smyth <tobs169@gmail.com>
2015-06-29 21:37:05 +0100

    Prod gulp

--- static/gulpfile.js
+++ static/gulpfile.js
@@ -4,27 +4,39 @@ var source = require('vinyl-source-stream');
 var watchify = require('watchify');
 var reactify = require('reactify');
 
-gulp.task('browserify', function () {
-    var bundler = browserify({
+var getBundler = function () {
+    return browserify({
         entries: ['./app/main.jsx'],
         transform: [reactify],
         debug: true,
         cache: {}, packageCache: {}, fullPaths: true
     });
-    var watcher = watchify(bundler);
+};
 
-    return watcher
-        .on('update', function(){
-            var updateStart = Date.now();
-            console.log('Gulping.');
-            watcher
-                .bundle()
+var bundleAndStream = function (bundler) {
+    return bundler.bundle()
                 .pipe(source('main.js'))
                 .pipe(gulp.dest('./build/'));
-        })
-        .bundle()
-        .pipe(source('main.js'))
-        .pipe(gulp.dest('./build/'));
+       
+};
+gulp.task('browserify', function () {
+    var bundler = getBundler(); 
+    var watcher = watchify(bundler);
+
+    return bundleAndStream(
+        watcher
+            .on('update', function(){
+                var updateStart = Date.now();
+                console.log('Gulping.');
+                bundleAndStream(watcher);
+        }));
+});
+
+gulp.task('browserifyOnce', function () {
+   return bundleAndStream(getBundler());
 });
 
 gulp.task('default',['browserify']);
+
+
+gulp.task('heroku:prod', ['browserifyOnce']);
